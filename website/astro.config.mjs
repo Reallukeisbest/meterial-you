4953f08a1f46896ed6bd090ba22fdb119b221047
Realluke <58954397+Reallukeisbest@users.noreply.github.com>
2022-12-17 15:02:48 -0600

    Sitemap

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import compress from "astro-compress";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [compress(), sitemap()]
+});
